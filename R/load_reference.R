f73e416c3de33a2d18b2e0275647102714174fc7
shumingyue <myshu0601@qq.com>
2021-09-06 14:07:25 +0800

    增加load reference

---
+++ R/load_reference.R
@@ -0,0 +1,15 @@
+#' LoadReference
+#'
+#' @param reference.gene.xls  File containing the real Gene Symbol (colname 'gene_symbol')
+#'
+#' @return ref_data a data frame consists of two columns ["fake_gene_symbol", "gene_symbol"].
+#' "fake_gene_symbol" are the genes that we match in the gene list, "gene_symbol" are the real names that we match
+#' @export
+#'
+#' @examples
+#' LoadReference("reference.gene.xls")
+LoadReference <- function(reference.gene.xls){
+  ref <- read.csv("test_data/reference.gene.xls", sep="\t", header=1, check.names=F)
+  newnames <- gsub("_","-", ref$gene_symbol)
+  ref_data <- data.frame("gene_symbol"=ref$gene_symbol, "fake_gene_symbol"=newnames)
+}
